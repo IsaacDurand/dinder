e3a18b7dcf7bf61287c8237721f7de5a0011ec86
Isaac <isaac.durand@gmail.com>
2018-10-20 12:26:13 -0700

    Incorporate form into index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,11 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
+    if @product.save
+      redirect_to products_url
+    else
+      head :bad_request # Is this the best error code to use?
+    end
   end
 
 private
